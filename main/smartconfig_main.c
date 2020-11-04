d5138ff0deadbf2bf0f8e31ecdf2b96fd4f975f3
Neel Oza <embeddedcoder@hotmail.com>
2020-11-05 02:22:05 +0530

    Added debug print and comment to understand the code

--- main/smartconfig_main.c
+++ main/smartconfig_main.c
@@ -28,12 +28,18 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
     switch(event->event_id) {
     case SYSTEM_EVENT_STA_START:
+        /* ESP32 station mode start */
+        ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
         xTaskCreate(smartconfig_example_task, "smartconfig_example_task", 4096, NULL, 3, NULL);
         break;
     case SYSTEM_EVENT_STA_GOT_IP:
+        /* ESP32 station got IP from connected AP */
+        ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
         xEventGroupSetBits(s_wifi_event_group, CONNECTED_BIT);
         break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
+        /* ESP32 station disconnected from AP */
+        ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
         esp_wifi_connect();
         xEventGroupClearBits(s_wifi_event_group, CONNECTED_BIT);
         break;
@@ -51,6 +57,7 @@ static void initialise_wifi(void)
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 
+    /* WiFi station mode */
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
     ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
     ESP_ERROR_CHECK( esp_wifi_start() );
@@ -60,24 +67,30 @@ static void sc_callback(smartconfig_status_t status, void *pdata)
 {
     switch (status) {
         case SC_STATUS_WAIT:
+            /* Waiting to start connect */
             ESP_LOGI(TAG, "SC_STATUS_WAIT");
             break;
         case SC_STATUS_FIND_CHANNEL:
+            /* Finding target channel */
             ESP_LOGI(TAG, "SC_STATUS_FINDING_CHANNEL");
             break;
         case SC_STATUS_GETTING_SSID_PSWD:
+            /* Getting SSID and password of target AP */
             ESP_LOGI(TAG, "SC_STATUS_GETTING_SSID_PSWD");
             break;
         case SC_STATUS_LINK:
+            /* Connecting to target AP */
             ESP_LOGI(TAG, "SC_STATUS_LINK");
             wifi_config_t *wifi_config = pdata;
             ESP_LOGI(TAG, "SSID:%s", wifi_config->sta.ssid);
             ESP_LOGI(TAG, "PASSWORD:%s", wifi_config->sta.password);
             ESP_ERROR_CHECK( esp_wifi_disconnect() );
+            /* ESP32 station interface */
             ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, wifi_config) );
             ESP_ERROR_CHECK( esp_wifi_connect() );
             break;
         case SC_STATUS_LINK_OVER:
+            /* Connected to AP successfully */
             ESP_LOGI(TAG, "SC_STATUS_LINK_OVER");
             if (pdata != NULL) {
                 uint8_t phone_ip[4] = { 0 };
@@ -94,14 +107,18 @@ static void sc_callback(smartconfig_status_t status, void *pdata)
 void smartconfig_example_task(void * parm)
 {
     EventBits_t uxBits;
+    /* Set protocol type of SmartConfig */
     ESP_ERROR_CHECK( esp_smartconfig_set_type(SC_TYPE_ESPTOUCH) );
+    /* Start SmartConfig */
     ESP_ERROR_CHECK( esp_smartconfig_start(sc_callback) );
     while (1) {
         uxBits = xEventGroupWaitBits(s_wifi_event_group, CONNECTED_BIT | ESPTOUCH_DONE_BIT, true, false, portMAX_DELAY);
         if(uxBits & CONNECTED_BIT) {
+            /* ESP32 Wifi connected with AP */
             ESP_LOGI(TAG, "WiFi Connected to AP");
         }
         if(uxBits & ESPTOUCH_DONE_BIT) {
+            /* Smart config task completed */
             ESP_LOGI(TAG, "smartconfig over");
             esp_smartconfig_stop();
             vTaskDelete(NULL);
